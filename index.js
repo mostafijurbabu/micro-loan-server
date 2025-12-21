aff287e21ae2e4fa4d8c507d8c318df890a2919b
mostafijurbabu <mostafijurbabu45@gmail.com>
2025-12-21 23:39:07 +0600

    modify backend

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const verifyFBToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
+    return res.status(401).send({ message: "unauthorized" });
   }
 
   try {
